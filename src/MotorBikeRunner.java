5b18e1478c0855dc8b1c03cd2e403d0791cdb77e
seoxuni <seoxuni@kakao.com>
2023-12-02 15:23:56 +0000

    update

---
+++ src/MotorBikeRunner.java
@@ -0,0 +1,17 @@
+package com.seojun.one;
+
+public class MotorBikeRunner {
+    public static void main(String[] args) {
+        MotorBike ducati = new MotorBike();
+        MotorBike honda = new MotorBike();
+
+        ducati.start();
+        honda.start();
+
+        ducati.setSpeed(100);
+        ducati.increaseSpeed(100);
+        ducati.decreaseSpeed(50);
+
+        System.out.println(ducati.getSpeed());
+    }
+}
