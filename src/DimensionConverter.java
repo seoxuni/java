c359b9ed3f2033530b1ff27ae61cc9d08d66ad4e
seoxuni <seoxuni@kakao.com>
2023-12-03 21:23:55 +0000

    update

--- src/DimensionConverter.java
+++ src/DimensionConverter.java
@@ -3,12 +3,12 @@ public class Dimension {
     private int inches;
 
     public Dimension(int inches) {
-        this.feet = inches / 12;
-        this.inches = inches % 12;
-
         if (inches < 0) {
             this.feet = -1;
             this.inches = -1;
+        } else {
+            this.feet = inches / 12;
+            this.inches = inches % 12;
         }
     }
 
