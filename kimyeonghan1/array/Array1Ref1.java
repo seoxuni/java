9cbff478057a3e7fc8081ace23bcbffdfb6fb543
seoxuni <seoxuni@kakao.com>
2023-12-19 05:28:26 +0900

    update

--- kimyeonghan1/array/Array1Ref1.java
+++ kimyeonghan1/array/Array1Ref1.java
@@ -14,7 +14,7 @@ public class Array1Ref1 {
         students[4] = 50;
 
         for (int i = 0; i < students.length; i++) {
-            System.out.println("학생" + i + " 점수: = " + students[i]);
+            System.out.println("학생" + (i + 1) + " 점수: = " + students[i]);
         }
 //        System.out.println("학생1 점수: = " + student1);
 //        System.out.println("학생2 점수: = " + student2);
